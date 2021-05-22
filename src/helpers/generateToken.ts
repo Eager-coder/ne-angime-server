f47e4a5f7ee03bd19a2240cc9eada05e7c988f0d
Turan Sultan <turan.sultann@gmail.com>
2021-05-23 01:49:28 +0600

    changed exp. time again

--- src/helpers/generateToken.ts
+++ src/helpers/generateToken.ts
@@ -11,7 +11,7 @@ export const generateTokens = (user: User) => {
 	const { user_id, username, email } = user
 
 	const access_token = sign({ user_id, email, username }, process.env.ACCESS_TOKEN_SECRET!, {
-		expiresIn: "10s",
+		expiresIn: "20s",
 	})
 	const refresh_token = sign({ user_id }, process.env.REFRESH_TOKEN_SECRET!, {
 		expiresIn: "30d",
