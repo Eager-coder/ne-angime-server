623f225185edf65dcc617ea8308c0ee42d42bd22
Turan Sultan <turan.sultann@gmail.com>
2021-05-22 16:56:08 +0600

    changed token exp. time

--- dist/helpers/generateToken.js
+++ dist/helpers/generateToken.js
@@ -5,10 +5,10 @@ var jsonwebtoken_1 = require("jsonwebtoken");
 var generateTokens = function (user) {
     var user_id = user.user_id, username = user.username, email = user.email;
     var accessToken = jsonwebtoken_1.sign({ user_id: user_id, email: email, username: username }, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "15m",
+        expiresIn: "5m",
     });
     var refreshToken = jsonwebtoken_1.sign({ user_id: user_id }, process.env.REFRESH_TOKEN_SECRET, {
-        expiresIn: "30m",
+        expiresIn: "10m",
     });
     return { accessToken: accessToken, refreshToken: refreshToken };
 };
